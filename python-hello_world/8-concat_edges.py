b0980c084e6850c409c9bae3eff90143db0bd9bb
Sona61 <sonanagdaliyeva4@gmail.com>
2026-04-13 16:40:39 +0000

    Add 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming language"
+str = ("Python is an interpreted, interactive, object-oriented "
+       "programming language")
 print(str[39:66] + " with " + str[:6])
