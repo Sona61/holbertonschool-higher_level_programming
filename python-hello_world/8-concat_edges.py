b476911ff62c530a8259331df3418919695411b8
Sona61 <sonanagdaliyeva4@gmail.com>
2026-04-13 16:45:05 +0000

    Add 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 str = ("Python is an interpreted, interactive, object-oriented "
        "programming language")
-print(str[39:66] + " with " + str[:6])
+str = str[39:66] + " with " + str[:6]
+print(str)
